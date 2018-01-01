deaa1779f7c5d0557d884fae7657c480a4553834
Burak KOSE <burakks41@gmail.com>
2018-01-01 17:43:57 +0100

    Add commit within parameter

--- src/main/scala/akka/stream/burakkose/solr/scaladsl/SolrSinkSettings.scala
+++ src/main/scala/akka/stream/burakkose/solr/scaladsl/SolrSinkSettings.scala
@@ -1,3 +1,7 @@
 package akka.stream.burakkose.solr.scaladsl
 
-trait SolrSinkSettings
+final case class SolrSinkSettings(bufferSize: Int = 10,
+                                  retryInterval: Int = 5000,
+                                  maxRetry: Int = 100,
+                                  retryPartialFailure: Boolean = true,
+                                  commitWithin: Int = -1)
