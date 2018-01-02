ee279baa5dbcbe4515514dff5c76bceb00e26ec9
Burak KOSE <burakks41@gmail.com>
2018-01-02 16:30:20 +0100

    Add solr sink and flow api

--- src/main/scala/akka/stream/burakkose/solr/scaladsl/SolrFlow.scala
+++ src/main/scala/akka/stream/burakkose/solr/scaladsl/SolrFlow.scala
@@ -1,3 +1,28 @@
 package akka.stream.burakkose.solr.scaladsl
 
-trait SolrFlow
+import akka.NotUsed
+import akka.stream.burakkose.solr.{
+  IncomingMessage,
+  IncomingMessagesResult,
+  SolrFlowStage
+}
+import akka.stream.scaladsl.Flow
+import org.apache.solr.client.solrj.SolrClient
+import org.apache.solr.common.SolrInputDocument
+
+object SolrFlow {
+  def create[T](collection: String, settings: SolrSinkSettings)(
+      implicit client: SolrClient,
+      binder: Option[T => SolrInputDocument] = None
+  ): Flow[IncomingMessage[T], IncomingMessagesResult[T], NotUsed] =
+    Flow
+      .fromGraph(
+        new SolrFlowStage[T](
+          collection,
+          client,
+          settings,
+          binder
+        )
+      )
+      .mapAsync(1)(identity)
+}
--- src/main/scala/akka/stream/burakkose/solr/scaladsl/SolrSink.scala
+++ src/main/scala/akka/stream/burakkose/solr/scaladsl/SolrSink.scala
@@ -1,3 +1,19 @@
 package akka.stream.burakkose.solr.scaladsl
 
-trait SolrSink
+import akka.Done
+import akka.stream.burakkose.solr.IncomingMessage
+import akka.stream.scaladsl.{Keep, Sink}
+import org.apache.solr.client.solrj.SolrClient
+import org.apache.solr.common.SolrInputDocument
+
+import scala.concurrent.Future
+
+object SolrSink {
+  def create[T](collection: String, settings: SolrSinkSettings)(
+      implicit client: SolrClient,
+      binder: Option[T => SolrInputDocument] = None
+  ): Sink[IncomingMessage[T], Future[Done]] =
+    SolrFlow
+      .create[T](collection, settings)
+      .toMat(Sink.ignore)(Keep.right)
+}
