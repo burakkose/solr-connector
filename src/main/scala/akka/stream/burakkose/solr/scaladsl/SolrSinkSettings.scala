be552bc4c5477356ad70cf84bd89a2b041df027e
Burak KOSE <burakks41@gmail.com>
2018-01-03 10:18:35 +0100

    Add documentation comment

--- src/main/scala/akka/stream/burakkose/solr/scaladsl/SolrSinkSettings.scala
+++ src/main/scala/akka/stream/burakkose/solr/scaladsl/SolrSinkSettings.scala
@@ -1,6 +1,8 @@
 package akka.stream.burakkose.solr.scaladsl
 
+//#sink-settings
 final case class SolrSinkSettings(bufferSize: Int = 10,
                                   retryInterval: Int = 5000,
                                   maxRetry: Int = 100,
                                   commitWithin: Int = -1)
+//#sink-settings
